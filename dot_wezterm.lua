35ab537c0901571196b35d7f752982cb9b0283a8
Tamika Nomara <taminomara@gmail.com>
2026-03-15 22:10:13 +0400

    update

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,13 +1,9 @@
 local wezterm = require('wezterm')
 
--- local theme = wezterm.plugin.require('https://github.com/neapsix/wezterm').main
--- return {
---     colors = theme.colors(),
---     window_frame = theme.window_frame(), -- needed only if using fancy tab bar
--- }
-
 local config = wezterm.config_builder()
 
 config.color_scheme = 'Default (dark) (terminal.sexy)'
+config.font_size = 14
+config.font = wezterm.font 'Cascadia Mono'
 
 return config
