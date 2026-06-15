aef27d65cec3d63a7b6851f4bf063054c7143844
Tamika Nomara <taminomara@gmail.com>
2026-06-15 19:23:30 +0400

    update

--- dot_wezterm.lua
+++
@@ -1,9 +0,0 @@
-local wezterm = require('wezterm')
-
-local config = wezterm.config_builder()
-
-config.color_scheme = 'Default (dark) (terminal.sexy)'
-config.font_size = 14
-config.font = wezterm.font 'Cascadia Mono'
-
-return config
--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,7 +1,7 @@
 set PATH $HOME/.local/bin $PATH
 
 # pnpm
-set -gx PNPM_HOME "/home/taminomara/.local/share/pnpm"
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
   set -gx PATH "$PNPM_HOME" $PATH
 end
